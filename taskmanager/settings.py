d7055369f4bd8b805ad545f95e0f2905d39390d6
samuelsamadikun <samuelsamadikun6@gmail.com>
2026-08-20 13:41:51 +0700

    1st project: adjustment and configuration for deployment #3 attempt

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mzx^_k))2y8p0-+d39cd-(9%1_dh0=-vpr5xzw)b5pxip%!@t2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
