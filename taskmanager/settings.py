aa92baadb2ed8ac4f6aca7ec10410b3ce3d2fd7d
samuelsamadikun <samuelsamadikun6@gmail.com>
2026-08-20 13:22:39 +0700

    1st project: adjusting and configuring to match the specification for deployment

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mzx^_k))2y8p0-+d39cd-(9%1_dh0=-vpr5xzw)b5pxip%!@t2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
